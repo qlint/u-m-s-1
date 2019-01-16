d26f8a6817ca63d392ab52e11f1a3803b35db029
andy15la <andrew_urban@ymail.com>
2019-01-16 21:45:13 +0300

    first commit, no customizations

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+  <!-- footer content -->
+  <footer>
+    <div class="pull-right">
+        <!-- Don't remove below text. Its againts copy right laws. -->
+    <strong>Smart Mind University Management System Version 1.0 - {{substr($idc,0,7)}}</strong> || Developed by <a href="#">Convergence Systems</a>
+    </div>
+    <div class="clearfix"></div>
+  </footer>
+  <!-- /footer content -->
+</div>
+</div>
